f330a34f587796f08902bf081fc3365e98168084
jwilll4455 <burakceylann@outlook.com>
2022-04-15 12:14:11 +0300

    Delete debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
