bd0cf178a4e69350bf93557c9d612b63d89d55f5
jwilll4455 <burakceylann@outlook.com>
2022-04-18 10:21:50 +0300

    Add list tile for list view

---
+++ lib/widgets/record_list_tile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RecordListTile extends StatelessWidget {
+  const RecordListTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(DateTime.now().toIso8601String()),
+      title: const Text('75'),
+      trailing: Row(children: const [
+        IconButton(
+          onPressed: null, icon: Icon(Icons.edit, color: Colors.grey,)),
+        IconButton(
+          onPressed: null, icon: Icon(Icons.delete, color: Colors.red,)),
+      ],),
+    );
+  }
+}
