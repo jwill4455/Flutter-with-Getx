bb33c3738e576639e3ffa606d031b006d1a0a6b6
jwilll4455 <burakceylann@outlook.com>
2022-04-18 15:39:51 +0300

    Delete Dummy datas and add addRecord method for add new records

--- lib/viewmodels/controller.dart
+++ lib/viewmodels/controller.dart
@@ -2,14 +2,11 @@ import 'package:flutterwithgetx/models/record.dart';
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
-
   var records = <Record>[
-
-    Record(dateTime: DateTime.now(), weight: 80, note: 'AAA'),
-    Record(dateTime: DateTime.now(), weight: 81, note: 'BBB'),
-    Record(dateTime: DateTime.now(), weight: 82, note: 'CCC'),
-    Record(dateTime: DateTime.now(), weight: 83, note: 'DDD'),
 
   ].obs;
 
+  void addRecord() {
+     records.add(Record(dateTime: DateTime.now(), weight: 80, note: 'XXX'));
+  }
 }
