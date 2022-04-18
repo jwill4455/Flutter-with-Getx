5afb7c22d0c735cb744a09c4f569a3e2cb6b2297
jwilll4455 <burakceylann@outlook.com>
2022-04-18 12:52:04 +0300

    Add Card for our list

--- lib/widgets/record_list_tile.dart
+++ lib/widgets/record_list_tile.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class RecordListTile extends StatelessWidget {
   const RecordListTile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Text(DateTime.now().toIso8601String()),
-      title: const Text('75'),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-        IconButton(
-          onPressed: null, icon: Icon(Icons.edit, color: Colors.grey,)),
-        IconButton(
-          onPressed: null, icon: Icon(Icons.delete, color: Colors.red,)),
-      ],),
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 8.0, bottom: 8),
+        child: ListTile(
+          leading: Text(DateFormat('EEE, MMM, d').format(DateTime.now())),
+          title: const Center(
+              child: Text('75', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          )),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.edit,
+                    color: Colors.grey,
+                  )),
+              IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
