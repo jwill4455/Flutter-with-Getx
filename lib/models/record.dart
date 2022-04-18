7546d83abf9f0e076b0fc46a76ab2ebd09c2adb1
jwilll4455 <burakceylann@outlook.com>
2022-04-18 13:29:14 +0300

    Add freezed annotation for generating auto json class

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -1,9 +1,18 @@
-class Record {
-  final int weight;
-  final DateTime dateTime;
-  final String? note;
-  final String? photoUrl;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Record({required this.dateTime, required this.weight, this.note, this.photoUrl});
+part 'record.freezed.dart';
+part 'record.g.dart';
+
+@freezed
+class Record with _$Record {
+
+  factory Record({
+    required DateTime dateTime,
+    required int weight,
+    String? note,
+    String? photoUrl
+  })= _Record;
+
+  factory Record.fromJson(Map<String, dynamic> json) => _$RecordFromJson(json);
 
 }
