ad90f9e9c2fdc108fea5e00d07b584b8e06723cd
jwilll4455 <burakceylann@outlook.com>
2022-04-19 11:18:02 +0300

    Set position and size of number picker

--- lib/views/add_record.dart
+++ lib/views/add_record.dart
@@ -10,7 +10,7 @@ class AddRecordView extends StatefulWidget {
 }
 
 class _AddRecordViewState extends State<AddRecordView> {
-  int _selectedValue = 0;
+  int _selectedValue = 70;
 
   @override
   Widget build(BuildContext context) {
@@ -24,24 +24,42 @@ class _AddRecordViewState extends State<AddRecordView> {
           Card(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16)),
-              child: Row(
-                children: [
-                  const Icon(
-                    FontAwesomeIcons.weightScale,
-                    size: 40,
-                  ),
-                  NumberPicker(
-                    axis: Axis.horizontal,
-                    minValue: 40,
-                    maxValue: 130,
-                    value: _selectedValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedValue = value;
-                      });
-                    },
-                  )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(
+                      FontAwesomeIcons.weightScale,
+                      size: 40,
+                    ),
+                    Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        NumberPicker(
+                          itemCount: 3,
+                          itemWidth: 80,
+                          itemHeight: 50,
+                          step: 1,
+                          axis: Axis.horizontal,
+                          minValue: 40,
+                          maxValue: 130,
+                          value: _selectedValue,
+                          onChanged: (value) {
+                            setState(() {
+                              _selectedValue = value;
+                            });
+                          },
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.grey),
+                          ),
+                        ),
+                         const Icon(FontAwesomeIcons.chevronUp, size: 14)
+                      ],
+                    )
+                  ],
+                ),
               )),
           Card(
             shape:
